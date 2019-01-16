77b6cb5a6dde8476997d11ca1dbfe6eece6e7c56
unknown <motifridman5@gmail.com>
2019-01-16 16:24:34 +0200

    Server added

---
+++ server/server.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+});
+
+var newUser = new User({
+    email: 'andrewj@gmail.com    ',
+    type: 'Admin'
+})
+
+newUser.save().then((doc) => {
+    console.log('Saved user', doc);
+}, (e) => {
+    console.log('Unable to save user', e.message);
+});
