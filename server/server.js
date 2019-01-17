5c91df550adb8878b46d5a4b04e0e9250b43fc61
unknown <motifridman5@gmail.com>
2019-01-17 12:47:24 +0200

    add id route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+var { ObjectID } = require('mongodb');
 
 var app = express();
 
@@ -23,9 +24,25 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
@@ -33,4 +50,4 @@ app.listen(3000, () => {
     console.log('Started on port 3000')
 });
 
-module.exports = {app};
+module.exports = { app };
