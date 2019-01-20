788f004398a63e973bf5fcf5f76daa115d0534b0
unknown <motifridman5@gmail.com>
2019-01-20 14:02:10 +0200

    Setup sepetate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
-//mongoose.connect(process.env.MONGODB_URI);
-//mongoose.connect('mongodb://motifridman:5678moti@ds026898.mlab.com:26898/node-course-db');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
