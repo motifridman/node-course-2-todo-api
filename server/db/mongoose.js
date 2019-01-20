2b3e696f5cd076ffcf508bf99c47bcb616915c28
unknown <motifridman5@gmail.com>
2019-01-20 09:39:29 +0200

    back to process.env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 //mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect('mongodb://motifridman:5678moti@ds159624.mlab.com:59624/node-course');
+//mongoose.connect('mongodb://motifridman:5678moti@ds026898.mlab.com:26898/node-course-db');
 
 module.exports = { mongoose };
