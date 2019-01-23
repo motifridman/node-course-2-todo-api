e8591965ba02ecb934fae8ce550d3128e9c1871e
unknown <motifridman5@gmail.com>
2019-01-23 11:24:47 +0200

    Add POST /users/login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 const request = require('supertest');
-var { ObjectID } = require('mongodb');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
